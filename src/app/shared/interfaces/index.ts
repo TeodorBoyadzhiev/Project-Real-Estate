62a4ac03e3e29be25b0c6856cdd5c0dd3bbab86f
Teodor Boyadzhiev <teodor1497@gmail.com>
2021-10-06 20:12:32 +0300

    front-end + back-end

--- src/app/shared/interfaces/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './apartment';
-export * from './user';
-export * from './comments';
--- src/app/shared/interfaces/user.ts
+++
@@ -1,8 +0,0 @@
-export interface IUser {
-    username: String,
-    email: String,
-    password: String,
-    repeatePassword: String,
-    rentedApartments: any[],
-    _id: String
-};
